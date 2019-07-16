6785aa25bf7fd1df67181d80202d3473580b63b0
asli ozmen <asliozmen@aslis-MacBook-Air.local>
2019-07-15 20:47:08 -0500

    second commit

---
+++ src/test/java/jenkinsPractice.java
@@ -0,0 +1,14 @@
+public class jenkinsPractice {
+
+    public static void main (String [] args){
+
+        for (int i=10; i>0 ; i--)
+            System.out.println("I need to be perfect in Jenkins");
+    }
+
+
+
+
+
+
+}
